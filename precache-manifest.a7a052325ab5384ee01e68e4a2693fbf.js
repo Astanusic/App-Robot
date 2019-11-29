32411098644cdce86c02d586a719908db14ed6e0
Astanusic <adrienpro.stanusic@gmail.com>
2019-11-29 17:39:46 +0100

    Updates

---
+++ precache-manifest.a7a052325ab5384ee01e68e4a2693fbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50329aa26ccb6a982e108da76724b8a2",
+    "url": "/App-Robot/index.html"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/App-Robot/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "0980f687757f6c03e023",
+    "url": "/App-Robot/static/css/main.0c4bba77.chunk.css"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/App-Robot/static/js/2.fbad7c07.chunk.js"
+  },
+  {
+    "revision": "0980f687757f6c03e023",
+    "url": "/App-Robot/static/js/main.552b865c.chunk.js"
+  },
+  {
+    "revision": "33fb26ff4a29d5bf4594",
+    "url": "/App-Robot/static/js/runtime-main.08355d0a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/App-Robot/static/media/SEGA.12a3db8b.woff"
+  }
+]);
